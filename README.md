02b407807edfbc7a0c49b6db044ab65bff8bface
mfatihb97 <mfatihbatur@gmail.com>
2024-09-17 01:58:35 +0300

    responsive design update 1

---
+++ README.md
@@ -0,0 +1,11 @@
+# Used Technologies
+
+This project is developed using the following technologies:
+
+- React.js
+- JavaScript
+- HTML
+- Ant Design
+
+You can access the live version of the project at: Vet Application :[(https://vet-app-frontend-nine.vercel.app/)]
+
---
+++ src/data/cityData.js
@@ -0,0 +1,13 @@
+const cities =
+[
+    'Adana', 'Adiyaman', 'Afyonkarahisar', 'Agri', 'Amasya', 'Ankara', 'Antalya', 'Artvin', 'Aydin', 'Balikesir',
+    'Bilecik', 'Bingöl', 'Bitlis', 'Bolu', 'Burdur', 'Bursa', 'Canakkale', 'Cankiri', 'Corum', 'Denizli', 'Diyarbakir',
+    'Edirne', 'Elazig', 'Erzincan', 'Erzurum', 'Eskisehir', 'Gaziantep', 'Giresun', 'Gümüshane', 'Hakkari', 'Hatay',
+    'Isparta', 'Mersin', 'İstanbul', 'İzmir', 'Kars', 'Kastamonu', 'Kayseri', 'Kirklareli', 'Kirsehir', 'Kocaeli',
+    'Konya', 'Kütahya', 'Malatya', 'Manisa', 'Kahramanmaras', 'Mardin', 'Mugla', 'Mus', 'Nevsehir', 'Nigde', 'Ordu',
+    'Rize', 'Sakarya', 'Samsun', 'Siirt', 'Sinop', 'Sivas', 'Tekirdag', 'Tokat', 'Trabzon', 'Tunceli', 'Sanliurfa',
+    'Usak', 'Van', 'Yozgat', 'Zonguldak', 'Aksaray', 'Bayburt', 'Karaman', 'Kirikkale', 'Batman', 'Sirnak', 'Bartin',
+    'Ardahan', 'Igdir', 'Yalova', 'Karabük', 'Kilis', 'Osmaniye', 'Düzce'
+  ];
+
+export default cities;
